67d47c70f7b7390ce8baf88f14a20cb91b4e0e41
banjocore <smurfomens@gmail.com>
2026-03-09 12:03:05 +0000

    está tudo a funconar DIREITO

--- script.js
+++ script.js
@@ -12,10 +12,33 @@ btn.addEventListener("click", () => {
 });
 
 function adicionarnota(noteText) {
+
     const li = document.createElement("li");
+    const btn1 = document.createElement("button");
+    const btn2 = document.createElement("button");
+    const span = document.createElement("span");
+
+    btn1.className = "btn btn-danger";
+    btn1.textContent = "Apagar";
+
+    btn2.className = "btn btn-success";
+    btn2.textContent = "editar";
     
     li.className = "list-group-item"; 
-    li.textContent = noteText;
+    span.textContent = noteText;
     
+    btn1.addEventListener("click", () => {
+        list.removeChild(li)
+    })
+
+    btn2.addEventListener("click", () => {
+        let editar = prompt();
+        span.textContent = editar;
+    })
+    li.appendChild(span);
+    li.appendChild(btn1);
+    li.appendChild(btn2);
+
     list.appendChild(li); 
+    
 }
